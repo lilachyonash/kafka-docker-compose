b4fc44d32aaf16d95a15fb4ca8dc389e7b369551
lilachyonash <lilach.yonash@tikalk.com>
2022-12-04 14:08:39 +0200

    producer

---
+++ src/main/java/lilachyonash/kafkadockercomppose/food/FoodOrderController.java
@@ -0,0 +1,27 @@
+package lilachyonash.kafkadockercomppose.food;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/order")
+public class FoodOrderController {
+
+    private final FoodOrderService foodOrderService;
+
+    @Autowired
+    public FoodOrderController(FoodOrderService foodOrderService) {
+        this.foodOrderService = foodOrderService;
+    }
+
+    @PostMapping
+    public String createFoodOrder(@RequestBody FoodOrder foodOrder) throws JsonProcessingException {
+        log.info("create food order request received");
+        return foodOrderService.createFoodOrder(foodOrder);
+    }
+
+
+}
